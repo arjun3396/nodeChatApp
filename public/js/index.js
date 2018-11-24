5ad491f8c36da686e160e908560c4875d63c4258
Arjun Vedak <arjun.vedak07@hotmail.com>
2018-11-24 09:48:52 +0530

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,15 +31,15 @@ var form = jQuery('#message-form');
 form.on('submit', function (e){
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from : 'User',
-    text : jQuery('[name=message]').val()
+    text : messageTextbox.val()
   }, function(){
-
+    messageTextbox.val('')
   });
-  $('#message-form').each(function(){
-    this.reset();
-});
+  // $('#message-form').each(function(){
+  //   this.reset();
 });
 
 var locationButton = jQuery('#send-location');
@@ -48,12 +48,18 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+
     alert('Unable to fetch location.');
   });
 });
